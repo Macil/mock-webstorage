09bd72f2235c85e0fa61803a14470d22e881a6be
Chris Cowan <agentme49@gmail.com>
2023-01-26 19:04:44 -0800

    update eslint and tsc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "mocha": true,
-    "node": true,
-    "es6": true
+  'parser': '@babel/eslint-parser',
+  'env': {
+    'browser': true,
+    'mocha': true,
+    'node': true,
+    'es6': true
   },
-  "extends": ["eslint:recommended"],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true
-    },
-    "sourceType": "module"
+  'extends': ['eslint:recommended'],
+  'parserOptions': {
+    'sourceType': 'module'
   },
-  "plugins": [],
-  "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single", "avoid-escape"],
-    "semi": ["error", "always"],
-    "no-var": ["error"],
-    "brace-style": ["error"],
-    "array-bracket-spacing": ["error", "never"],
-    "block-spacing": ["error", "always"],
-    "no-spaced-func": ["error"],
-    "no-whitespace-before-property": ["error"],
-    "space-before-blocks": ["error", "always"],
-    "keyword-spacing": ["error"]
+  'plugins': [],
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single', 'avoid-escape'],
+    'semi': ['error', 'always'],
+    'no-var': ['error'],
+    'brace-style': ['error'],
+    'array-bracket-spacing': ['error', 'never'],
+    'block-spacing': ['error', 'always'],
+    'no-spaced-func': ['error'],
+    'no-whitespace-before-property': ['error'],
+    'space-before-blocks': ['error', 'always'],
+    'keyword-spacing': ['error']
   }
 };
